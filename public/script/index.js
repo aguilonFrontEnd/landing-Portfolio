7914d7eae9ad1904f9029d81e0930373f50490de
Alejandro Aguilon <aguilondevelopsoft@gmail.com>
2026-04-12 14:22:44 -0500

    fix: update script path and add type module v4

--- public/script/index.js
+++
@@ -1,9 +0,0 @@
-  // Importar directamente
-  import { initBreakpoint } from '/src/script/index.js';
-  
-  // Ejecutar
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initBreakpoint);
-  } else {
-    initBreakpoint();
-  }
